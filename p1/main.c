7815d4debe159bc9b626a91c73a5faf539178959
Rafael Gaspar Ferreira <rafael.gaspar@tecnico.ulisboa.pt>
2022-03-28 13:03:05 +0100

    case a and haf of case l

--- p1/main.c
+++ p1/main.c
@@ -15,10 +15,12 @@ int main(){
             case 'a':{
                 char id[4], country[MAX_COUTRY_NAME], city[MAX_CITY_NAME];
                 scanf(" %s %s %[^\n]", id, country, city);
-                add_airport(id, country, city, airports, airports_counter);
+                add_airport(id, country, city, airports, airports_counter++);
                 break;
             }
             case 'l':
+                print_all_airports(airports_counter);
+                break;
             case 'v':
             case 'p':
             case 'c':
@@ -30,13 +32,36 @@ int main(){
         }
     }
 }
-
-void add_airport(char id[], char country[], char city[], Airport airports[], int counter){
+void add_airport(char id[], char country[], char city[], Airport airports[], 
+int counter){
+    int i = 0;
     if(counter >= MAX_AIRPORTS){
-        printf("too many airports");
+        printf("too many airports\n");
+        return;
+    }
+
+    while(id[i] != '\0'){
+        if (id[i] < 'A' || id[i] > 'Z'){
+            printf("invalid airport ID\n");
+            return;
+        }
+        i++;
+    }
+    for(i = 0; i < counter; i++){
+        if(!(strcmp(id, airports[i].id))){
+            return;
+        }
     }
     strcpy(airports[counter].id, id);
     strcpy(airports[counter].country, country);
     strcpy(airports[counter].city, city);
-    printf("airport %s\n", airports[airports_counter++].id);
+    /*Algoritmo de ordenação por ordem alfabética*/
+    printf("airport %s\n", airports[counter].id);
+}
+void print_all_airports(int counter){
+    int i;
+    for(i = 0; i < counter; i++){
+        printf("%s %s %s\n", airports[i].id, airports[i].city, 
+        airports[i].country);
+    }
 }
--- p1/main.h
+++ p1/main.h
@@ -1,12 +1,23 @@
 #include<stdio.h>
-#include <string.h>
+#include<string.h>
+#include<stdlib.h>
+#include<ctype.h>
 
 #define MAX_AIRPORTS 40
 #define MAX_FLIGHTS 30000
 #define MAX_CITY_NAME 50
 #define MAX_COUTRY_NAME 30
 
+typedef struct Date{
+    int day;
+    int moth;
+    int year;
+} Date;
 
+typedef struct Hour{
+    int hours;
+    int minutes;
+} Hour;
 
 typedef struct Airport{
     char id[4];
@@ -15,3 +26,5 @@ typedef struct Airport{
 } Airport;
 
 void add_airport(char id[], char country[], char city[], Airport airports[], int counter);
+
+void print_all_airports(int counter);
